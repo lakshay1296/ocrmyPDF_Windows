2bb4a91174d9c7e3695be29f9ed1fce31ddbb225
lakshay1296 <lakshay1296@gmail.com>
2020-01-10 18:13:05 +0530

    second commit

--- Main.py
+++ Main.py
@@ -1,4 +1,5 @@
 import ocrmypdf
 
 ocrmypdf.ocr(input_file="C:\\Users\lakshay.saini\Desktop\Med Legal\PDF\PDF\image\MW Complete Pain Solutions.pdf",
-             output_file="C:\\Users\lakshay.saini\Desktop\Med Legal\PDF\PDF\image\MW Complete Pain Solutions_ocr.pdf", force_ocr=True)
+             output_file="C:\\Users\lakshay.saini\Desktop\Med Legal\PDF\PDF\image\MW Complete Pain Solutions_ocr.pdf",
+             force_ocr=True)
