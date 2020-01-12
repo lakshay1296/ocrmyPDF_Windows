6ce27d0011872c49920f9dd019a489eaec76ef3e
lakshay1296 <lakshay1296@gmail.com>
2020-01-12 17:28:30 +0530

    added virtual env containing scripts

--- Main.py
+++ Main.py
@@ -1,8 +1,8 @@
 import ocrmypdf
 
 def ocr():
-    ocrmypdf.ocr(input_file="C:\\D_Drive\Software_Developer_Resume_Lakshay.pdf",
-             output_file="C:\\D_Drive\Software_Developer_Resume_Lakshay_ocr.pdf",
+    ocrmypdf.ocr(input_file="C:\\D_Drive\scansmpl.pdf",
+             output_file="C:\\D_Drive\scansmpl_ocr.pdf",
              force_ocr=True)
 
 if __name__ == "__main__":
